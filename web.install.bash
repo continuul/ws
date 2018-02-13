0d7bb283293c1afba349392ae84859d636ef138f
Robert Buck <bbuck@cloudhealthtech.com>
2018-02-13 12:08:36 +0000

    fix web deploy tagging

--- web.install.bash
+++ web.install.bash
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+: ${TAG:="stable"}
+
 TMPDIR=/tmp/ws.$RANDOM
 mkdir $TMPDIR
 (
-cd $TMPDIR
-curl -Lo $TMPDIR/ws.tgz https://github.com/continuul/ws/releases/download/v1.0/ws.tgz
-tar xzf ws.tgz
-cat install | bash
+  cd $TMPDIR
+  curl -Lo $TMPDIR/ws.tgz https://github.com/continuul/ws/releases/download/$TAG/ws.tgz
+  tar xzf ws.tgz
+  cat install | bash
 )
 rm -fr $TMPDIR
